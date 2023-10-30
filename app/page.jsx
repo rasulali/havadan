a5ec0dc41a362eebe34cbdca8758295ecf8b58ed
rasulali <resul.eliyev2000@mail.ru>
2023-10-31 00:45:38 +0400

    layout work and tailwind config

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,24 @@
+'use client'
+import { useState } from "react"
+
 const Main = () => {
+  const [drawerState, setDrawerState] = useState(false)
   return (
-    <h1 className="text-red-50 text-xl" >The Weather is good today!</h1>
+    // Main wrapper grid
+    <div className="flex flex-col h-screen justify-center bg-gray-200 relative overflow-hidden">
+      {/*Current, hourly and weekly weather*/}
+      <div className="h-2/3">
+      </div>
+      {/* Additional information section*/}
+      <div className={`w-screen h-4/5 rounded-t-3xl absolute bottom-0 ${drawerState ?
+        'translate-y-0' : 'translate-y-2/3'} transition bg-white`}>
+        <h1 className="text-xl text-center px-4 py-2 border-2 rounded-full border-black w-24 mx-auto mt-4 cursor-pointer"
+          onClick={() => { setDrawerState(!drawerState) }}
+        >
+          {drawerState ? "Down" : "Up"}
+        </h1>
+      </div>
+    </div>
   )
 }
 export default Main
